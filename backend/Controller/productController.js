60fba59855c29399cd1997c5c650f9e6741cf424
manthan3678 <manthangedam198@gmail.com>
2025-05-30 21:11:58 +0530

    CRUD Done

--- backend/Controller/productController.js
+++ backend/Controller/productController.js
@@ -1,5 +1,7 @@
 const slugify = require("slugify");
 const productModel = require("../Models/productSchema");
+const { param } = require("../Routes/productRoutes");
+// Create Product  !!!!!!!!!!!!!
 const createProductController = async (req, res) => {
   try {
     const { name, slug, description, price, quantity, photo, shipping } =
@@ -12,10 +14,6 @@ const createProductController = async (req, res) => {
       case !description:
         return res.status(500).send({
           error: "description is require",
-        });
-      case !category:
-        return res.status(500).send({
-          error: "category is require",
         });
       case !quantity:
         return res.status(500).send({
@@ -47,6 +45,7 @@ const createProductController = async (req, res) => {
       products,
     });
   } catch (error) {
+    console.log(error);
     return res.send({
       success: false,
       message: "Error In Product Create",
@@ -54,5 +53,98 @@ const createProductController = async (req, res) => {
     });
   }
 };
+// Get All Product !!!!!!!!!!!!!
+const getProductController = async (req, res) => {
+  try {
+    const products = await productModel.find({});
 
-module.exports = { createProductController };
+    return res.send({
+      success: true,
+      total: products.length,
+      message: "All Product Get SuccessFully",
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.send({
+      success: false,
+      error,
+      message: "Error In Getting Product Details ",
+    });
+  }
+};
+//  Delete Product !!!!!!!!!!!!!
+const deleteProductController = async (req, res) => {
+  try {
+    await productModel.findByIdAndDelete(req.params.pid);
+    return res.send({
+      success: true,
+      message: "Product Delete SuccessFully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.send({
+      success: false,
+      error,
+      message: " Error in Product Deleted ",
+    });
+  }
+};
+// Update Product !!!!!!!!!!!
+const updateProductController = async (req, res) => {
+  try {
+    const { name, slug, description, price, quantity, photo, shipping } =
+      req.body;
+    switch (true) {
+      case !name:
+        return res.status(500).send({
+          error: "Name is require",
+        });
+      case !description:
+        return res.status(500).send({
+          error: "description is require",
+        });
+      case !quantity:
+        return res.status(500).send({
+          error: "quantity is require",
+        });
+      case !price:
+        return res.status(500).send({
+          error: "price is require",
+        });
+      case !photo:
+        return res.status(500).send({
+          error: "photo is require less them 1mb",
+        });
+    }
+    //
+    const products = await productModel.findByIdAndUpdate(
+      req.params.pid,
+      {
+        ...req.body,
+        slug: slugify(name),
+      },
+      { new: true }
+    );
+    await products.save();
+    return res.send({
+      success: true,
+      message: "Product Update SuccessFully",
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.send({
+      success: false,
+      message: "Error In Updating Product",
+      error,
+    });
+  }
+};
+//
+module.exports = {
+  createProductController,
+  getProductController,
+  deleteProductController,
+  updateProductController,
+};
--- backend/Routes/productRoutes.js
+++ backend/Routes/productRoutes.js
@@ -1,11 +1,27 @@
 const Router = require("express").Router();
 const { checkUserAuth, checkAdmin } = require("../middleware/auth-middleware");
-const { createProductController } = require("../Controller/productController");
+const {
+  createProductController,
+  getProductController,
+  deleteProductController,
+  updateProductController,
+} = require("../Controller/productController");
+// Post Method
 Router.post(
   "/create-product",
   checkUserAuth,
   checkAdmin,
   createProductController
 );
-
+// Get Method
+Router.get("/get-product", getProductController);
+// Delete Method
+Router.delete("/delete-product/:pid", deleteProductController);
+// Update Method
+Router.put(
+  "/update-product/:pid",
+  checkUserAuth,
+  checkAdmin,
+  updateProductController
+);
 module.exports = Router;
