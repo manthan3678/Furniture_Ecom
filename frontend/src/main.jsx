ca5ff9f925d1e41e54b909110ee12e6bf8ea4ef4
manthan3678 <manthangedam198@gmail.com>
2025-05-31 12:47:56 +0530

    Ui Product Created form

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,14 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { ToastContainer } from "react-toastify";
 import { Authorization } from "./context/Authorization.jsx";
 import { CartProvider } from "./context/CartContext.jsx";
 createRoot(document.getElementById("root")).render(
   <Authorization>
     <CartProvider>
       <App />
+      <ToastContainer />
     </CartProvider>
   </Authorization>
 );
--- frontend/src/pages/admin/AdminProducts.jsx
+++ frontend/src/pages/admin/AdminProducts.jsx
@@ -1,10 +1,142 @@
 import React from "react";
 import AdminLayout from "../../components/admin/AdminLayout";
-
+import { useState } from "react";
+import axios from "axios";
+import { useAuth } from "../../context/Authorization";
+import { toast } from "react-toastify";
 const AdminProducts = () => {
+  // const [categories, setCategories] = useState([]);
+  const [photo, setPhoto] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  // const [category, setCategory] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [price, setPrice] = useState("");
+  const [shipping, setShipping] = useState("");
+  const [auth, setAuth] = useAuth();
+
+  //
+  const handleCreate = async (e) => {
+    e.preventDefault();
+    try {
+      // const productData = new FormData();
+      // // photo less then 1mb
+      // productData.append("photo", photo);
+      // productData.append("name", name);
+      // productData.append("description", description);
+      // productData.append("price", price);
+      // productData.append("quantity", quantity);
+      // productData.append("shipping", shipping);
+
+      const { data } = await axios.post(
+        "http://localhost:5000/api/product/create-product",
+        {
+          name,
+          photo,
+          description,
+          price,
+          quantity,
+          shipping,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${auth?.token}`,
+          },
+        }
+      );
+      if (data?.success) {
+        toast.success("Product Created SuccessFully");
+      } else {
+        toast.error("Something Went Wrong");
+      }
+      setName("");
+      setPrice("");
+      setPhoto("");
+      setDescription("");
+      setQuantity("");
+      setShipping("");
+    } catch (error) {
+      console.log("Something error in Creating", error);
+    }
+  };
   return (
     <AdminLayout>
-      <h1>Products</h1>
+      <div className="container-fluid  ">
+        <div className="row">
+          <div className="d-flex justify-content-center align-items-center flex-column">
+            <h1>Create Products</h1>
+            <div className="mt-1 w-75">
+              {/* isko bad me dekhna iska data nhi ara  */}
+
+              {/* image section */}
+              <div className="mb-3">
+                <input
+                  type="text"
+                  value={photo}
+                  placeholder="Give the link of Image"
+                  className="form-control"
+                  onChange={(e) => setPhoto(e.target.value)}
+                />
+              </div>
+
+              {/*  */}
+              <div className="mb-3">
+                <input
+                  type="text"
+                  value={name}
+                  placeholder="Write A Name"
+                  className="form-control"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              {/*  */}
+              <div className="mb-3">
+                <textarea
+                  type="text"
+                  value={description}
+                  name=""
+                  id=""
+                  placeholder="Write A Description"
+                  className="form-control"
+                  onChange={(e) => setDescription(e.target.value)}
+                ></textarea>
+              </div>
+              <div className="mb-3">
+                <input
+                  type="number"
+                  value={price}
+                  placeholder="Write A Price"
+                  className="form-control"
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="number"
+                  value={quantity}
+                  placeholder="Write A Quantity"
+                  className="form-control"
+                  onChange={(e) => setQuantity(e.target.value)}
+                />
+              </div>
+              <div className="mb-3 ">
+                <input
+                  type="number"
+                  value={shipping}
+                  placeholder="Write Yes Or No"
+                  className="form-control"
+                  onChange={(e) => setShipping(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <button className="btn btn-success" onClick={handleCreate}>
+                  CREATE PRODUCT
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </AdminLayout>
   );
 };
